4196ba735ac3cc0a0c66ae87a64473ddfb109b8b
Shaverda <shaverda@gmail.com>
2017-06-04 13:00:10 -0500

    added profile create

--- App/Containers/ProfileEntry.js
+++ App/Containers/ProfileEntry.js
@@ -1,54 +1,116 @@
 import React from 'react'
-import { ScrollView, Text, Image, View } from 'react-native'
-
+import { ScrollView, Text, Image, View, TouchableHighlight, StyleSheet } from 'react-native'
+import form from 'tcomb-form-native'
 import { Images } from '../Themes'
 
+var Form = form.form.Form;
 // Styles
 import styles from './Styles/ProfileEntryStyles'
 
-export default class ProfileEntry extends React.Component {
-    render () {
+
+var User = form.struct({
+  email_address: form.String,              
+  username: form.String,
+  password: form.String      
+});
+var options = {};
+
+var ProfileEntry = React.createClass({
+    getInitialState() {
+    return {
+            email_address: "",              
+            username: "",
+            password: "",
+            position: ""
+    };
+    },
+
+
+    componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          position: position
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+    },
+
+    onPress: function () {
+        // call getValue() to get the values of the form
+        var value = this.refs.form.getValue();
+        if (value) { // if validation fails, value will be null
+          console.log(value); // value here is an instance of Person
+        }
+        console.log(this.state.position);
+
+        fetch('http://10.5.31.104:8080/accounts/create', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            accountName: value.username,
+            email: value.email_address,
+            password: value.password, 
+            location: {
+                x: this.state.latitude,
+                y: this.state.longitude
+            }
+          })
+        }).then((response) => {
+          console.log(response);
+        })
+  },
+
+
+
+    render: function () { 
         return (
-                <View style={styles.mainContainer}>
-                <ScrollView style={styles.container}>
-                
-                <View style={styles.section} >
-                <Text style={styles.sectionText}>
-                Welcome to Garden to Garden!
-                Whether you garden yourself, want fresh veggies,
-                or any other reason, we are glad you are here.
-                
-                This application is based off locale - why drive
-                to swap veggies when your neighbor down the street
-                has exactly what you want?
-                
-                On your profile, you upload a personal picture
-                which ideally includes your face.  Once that's
-                completed, you put in your zip code and start
-                listed the vegetables, spices, fruits, herbs,
-                and flowers you are growing.  Immediately next
-                to your column of items you are growing is a
-                "ready by" section.  This lets everyone know
-                when the item you are growing is ready to swap.
-                
-                Some other aspects include the quantity, quality,
-                and image uploading section.
-                
-                In the future we hope to add a section of what
-                you are searching for.  That way instead of 
-                just swiping on the people within a certain 
-                circumference, you can filter based off a 
-                specific gardened item.  Or you can just 
-                see what your neighbors are offering.  
-                Either way, we appreciate you and your 
-                patience and hope to grow into the perfect
-                tool for you.  
-
-                </Text>
-                </View>
-
-                </ScrollView>
+                <View style={form_styles.container}>
+                     <Form
+                      ref="form"
+                      type={User}
+                        options={options}
+                        />
+                    <TouchableHighlight  style={form_styles.button} onPress={this.onPress} underlayColor='#99d9f4'>
+                        <Text style={form_styles.buttonText}>Submit</Text>
+                    </TouchableHighlight>
                 </View>
                 )
     }
-}
+});
+
+var form_styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+  title: {
+    fontSize: 30,
+    alignSelf: 'center',
+    marginBottom: 30
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  }
+});
+
+export default ProfileEntry;
